6ee646eefa4d24a3428151b65725cdda8603af78
Mai Pham <maitphm@gmail.com>
2020-10-27 05:11:13 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  def square_array(numbers)
+  
+    squared_numbers = []
+	  numbers.each do |num|
+      squared_numbers << (num ** 2)
+  end
+  return squared_numbers
+  
+end
 end
